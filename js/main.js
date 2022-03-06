ce279f27006e6cfb0e6adf76c80e37e0b72d87f5
GalaxyLinus <57060126+GalaxyLinus@users.noreply.github.com>
2022-03-06 18:51:58 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -11,9 +11,11 @@ about.addEventListener('click', () => {
         background: '#00aa00',
         width: '400px',
         height: '400px',
+        x: 50,
+        y: 50,
         top: 50,
         right: 50,
-        bottom: 0,
+        bottom: 50,
         left: 50,
         mount: aboutContent
     })
@@ -25,30 +27,62 @@ contact.addEventListener('click', () => {
         background: '#00aa00',
         width: '400px',
         height: '400px',
+        x: 100,
+        y: 100,
         top: 150,
         right: 50,
         bottom: 0,
         left: 150,
         mount: contactContent
     })
-
-    contactBox.top = 50;
-    contactBox.left = 50;
 })
 
 terminal.addEventListener('click', () => {
+    const terminal = $('#terminal-content').terminal({
+        help: function() {
+            this.echo('\nTry these commands:');
+            this.echo('help:	see this page');
+            this.echo('test:	self explanatory');
+            this.echo('cat:	get picture of cute kitty');
+
+            this.echo('\n');
+        },
+    
+        cat: function() {
+            this.echo($('<img src="https://cataas.com/cat?ignore=' + Math.floor(Math.random()* 1000) +'" width="200">'));
+            this.echo('\n');
+        },
+
+        test: function() {
+            this.echo('this is a test');
+        }
+    
+    
+    
+    
+    
+    }, {
+        greetings: '',
+        prompt: 'linusx:~$ '
+    });
+
+
     const terminalBox = new WinBox({
         title: 'Terminal',
         background: '#00aa00',
         width: '400px',
         height: '400px',
-        top: 200,
+        x: 200,
+        y: 200,
+        top: 50,
         right: 50,
-        bottom: 0,
-        left: 200,
-        mount: terminalContent
+        bottom: 50,
+        left: 50,
+        mount: terminalContent,
+        onclose: function(){
+            terminal.exec('clear');
+        }
     })
-
-    contactBox.top = 50;
-    contactBox.left = 50;
 })
+
+
