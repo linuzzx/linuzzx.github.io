9f7af3219b72697e7029f845587858656859f4b4
GalaxyLinus <57060126+GalaxyLinus@users.noreply.github.com>
2022-03-09 16:38:09 +0100

    fix terminal not closing + fix multiple cloned boxes open at once

--- js/main.js
+++ js/main.js
@@ -5,56 +5,78 @@ const aboutContent = document.querySelector('#about-content')
 const contactContent = document.querySelector('#contact-content')
 const terminalContent = document.querySelector('#terminal-content')
 
+let aboutOpen = false;
 about.addEventListener('click', () => {
-    new WinBox({
-        title: 'About Me',
-        background: '#00aa00',
-        width: '400px',
-        height: '400px',
-        x: 50,
-        y: 50,
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10,
-        mount: aboutContent
-    })
+    if (aboutOpen == false) {
+        aboutOpen = true;
+        const aboutBox = new WinBox({
+            title: 'About Me',
+            background: '#00aa00',
+            width: '400px',
+            height: '400px',
+            x: 50,
+            y: 50,
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10,
+            mount: aboutContent,
+            onclose: function(){
+                aboutOpen = false;
+            }
+        })
+    }
 })
 
+let contactOpen = false;
 contact.addEventListener('click', () => {
-    new WinBox({
-        title: 'Contact Me',
-        background: '#00aa00',
-        width: '400px',
-        height: '400px',
-        x: 100,
-        y: 100,
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10,
-        mount: contactContent
-    })
+    if (contactOpen == false) {
+        contactOpen = true;
+        const contactBox = new WinBox({
+            title: 'Contact Me',
+            background: '#00aa00',
+            width: '400px',
+            height: '400px',
+            x: 100,
+            y: 100,
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10,
+            mount: contactContent,
+            onclose: function(){
+                contactOpen = false;
+            }
+        })
+    }
 })
 
+let terminalOpen = false;
 terminal.addEventListener('click', () => {
-    makeTerminal();
+    if (terminalOpen == false) {
+        terminalOpen = true;
 
+        const terminalEngine = $('#terminal-content').terminal(terminalCommands, {
+            greetings: '',
+            prompt: 'linusx:~$ '
+        });
 
-    const terminalBox = new WinBox({
-        title: 'Terminal',
-        background: '#00aa00',
-        width: '400px',
-        height: '400px',
-        x: 200,
-        y: 200,
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10,
-        mount: terminalContent,
-        onclose: function(){
-            terminalBox.exec('clear');
-        }
-    });
+        const terminalBox = new WinBox({
+            title: 'Terminal',
+            background: '#00aa00',
+            width: '400px',
+            height: '400px',
+            x: 200,
+            y: 200,
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10,
+            mount: terminalContent,
+            onclose: function(){
+                terminalEngine.exec('clear');
+                terminalOpen = false;
+            }
+        });
+    }
 })
